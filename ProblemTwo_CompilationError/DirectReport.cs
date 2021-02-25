5c5c0e60cf84d8dfa216cc3699b4ea33fa1c0f7e
99003668 <78849805+99003668@users.noreply.github.com>
2021-02-25 17:28:26 +0530

    Update DirectReport.cs

--- ProblemTwo_CompilationError/DirectReport.cs
+++ ProblemTwo_CompilationError/DirectReport.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,15 +6,30 @@ using System.Threading.Tasks;
 
 namespace ProblemTwo_CompilationError
 {
-    class DirectReport
+    class DirectReport:Employee
     {
+        private object p;
+        private string v;
+        private static String eName;
+        private static int eLevel;
+        private static DateTime eDoJ;
+
         public Employee ReportsTo { get; set; }
         public string ProjectCode { get; set; }
+        public static int eID { get; }
+
+        //public DirectReport(int l, DateTime doj, int diff)
 
         public DirectReport(Employee managerEmpObj,string pCode) :base(eID,eName,eLevel,eDoJ)
         {
             ReportsTo = managerEmpObj;
             ProjectCode = pCode;
+        }
+
+        public DirectReport(int eID, string eName, int eLevel, DateTime eDoJ, object p, string v) : base(eID, eName, eLevel, eDoJ)
+        {
+            this.p = p;
+            this.v = v;
         }
     }
 }
